e6f96719a27df1c224e2d98d249b6cb0ea99316a
Yuli sotnikov <yulisotnikov123@gmil.com>
2024-06-14 21:14:35 +0300

    caloriesController update

--- controllers/caloriesController.js
+++ controllers/caloriesController.js
@@ -1,8 +1,16 @@
-const Calorie = require('../models/calorie');
+const User = require('../models/user');
+const Calorie = require('../models/Calorie');
 
 exports.addCalorie = async (req, res) => {
     try {
         const { user_id, year, month, day, description, category, amount } = req.body;
+
+        // Check if the user with the given user_id exists
+        const user = await User.findOne({ id: user_id });
+        if (!user) {
+            return res.status(404).json({ error: 'User not found' });
+        }
+
         const id = new Date().getTime(); // Generate a unique ID
         const newCalorie = new Calorie({ user_id, year, month, day, id, description, category, amount });
         await newCalorie.save();
