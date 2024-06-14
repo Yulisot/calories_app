43effdf5d95b9ed668205ded48ab242935caa6ad
Yuli sotnikov <yulisotnikov123@gmil.com>
2024-06-14 21:30:54 +0300

    repoetController update

--- routes/report.js
+++ routes/report.js
@@ -2,6 +2,7 @@ const express = require('express');
 const router = express.Router();
 const reportController = require('../controllers/reportController');
 
+// Define the route handler for GET requests to /report
 router.get('/report', reportController.getReport);
 
 module.exports = router;
