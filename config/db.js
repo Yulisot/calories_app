1b3d5fd603a9e4035556dee074cca38634b0e646
Yuli sotnikov <yulisotnikov123@gmil.com>
2024-06-14 20:38:54 +0300

    db.js update

--- config/db.js
+++ config/db.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const connectDB = async () => {
     try {
-        await mongoose.connect('mongodb+srv://Calories:App@cluster0.u5clyiu.mongodb.net/', {
+        await mongoose.connect('mongodb+srv://Calories:App@cluster0.u5clyiu.mongodb.net/calories_db', {
             useNewUrlParser: true,
             useUnifiedTopology: true,
         });
