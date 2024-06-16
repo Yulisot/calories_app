8279a263b74a8a48889efb90195cf0f67491950b
Yuli sotnikov <yulisotnikov123@gmil.com>
2024-06-16 12:58:02 +0300

    fixed date for user

--- models/Yuli Sotnikov 208471391 Ravit Suissa 207386491 user.js
+++ models/Yuli Sotnikov 208471391 Ravit Suissa 207386491 user.js
@@ -1,10 +1,28 @@
 const mongoose = require('mongoose');
+const moment = require('moment');
 
 const UserSchema = new mongoose.Schema({
     id: Number,
     first_name: String,
     last_name: String,
     birthday: Date
+});
+
+// Virtual field for formatted birthday
+UserSchema.virtual('formattedBirthday').get(function() {
+    return moment(this.birthday).format('MMMM Do, YYYY'); // Using moment.js
+
+});
+
+// Customize the JSON output
+UserSchema.set('toJSON', {
+    virtuals: true,
+    transform: (doc, ret) => {
+        delete ret._id;
+        delete ret.__v;
+        ret.birthday = ret.formattedBirthday; // Replace the birthday with the formatted one
+        delete ret.formattedBirthday; // Remove the virtual field from the final output
+    }
 });
 
 module.exports = mongoose.model('User', UserSchema);
