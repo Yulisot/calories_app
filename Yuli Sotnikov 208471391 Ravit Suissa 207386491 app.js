0564f846d26d175734b2496929af8baeb2843ecf
Yuli sotnikov <yulisotnikov123@gmil.com>
2024-06-16 12:20:53 +0300

    package.json update

--- Yuli Sotnikov 208471391 Ravit Suissa 207386491 app.js
+++ Yuli Sotnikov 208471391 Ravit Suissa 207386491 app.js
@@ -8,15 +8,15 @@ const app = express();
 connectDB();
 
 // Middleware
-yuliSotnikov208471391RavitSuissa207386491App.use(bodyParser.json());
+app.use(bodyParser.json());
 
 // Routes
-yuliSotnikov208471391RavitSuissa207386491App.use('/', require('./routes/Yuli Sotnikov 208471391 Ravit Suissa 207386491 calories'));
-yuliSotnikov208471391RavitSuissa207386491App.use('/', require('./routes/Yuli Sotnikov 208471391 Ravit Suissa 207386491 users'));
-yuliSotnikov208471391RavitSuissa207386491App.use('/', require('./routes/Yuli Sotnikov 208471391 Ravit Suissa 207386491 report'));
+app.use('/', require('./routes/Yuli Sotnikov 208471391 Ravit Suissa 207386491 calories'));
+app.use('/', require('./routes/Yuli Sotnikov 208471391 Ravit Suissa 207386491 users'));
+app.use('/', require('./routes/Yuli Sotnikov 208471391 Ravit Suissa 207386491 report'));
 
 // About endpoint
-yuliSotnikov208471391RavitSuissa207386491App.get('/about', (req, res) => {
+app.get('/about', (req, res) => {
     const developers = [
         { firstname: 'Yuli', lastname: 'Sotnikov', id: 208471391, email: 'yulisotnikov123@gmail.com' },
         { firstname: 'Ravit', lastname: 'Suissa', id: 207386491, email: 'ahualravit@gmail.com' }
@@ -26,6 +26,6 @@ yuliSotnikov208471391RavitSuissa207386491App.get('/about', (req, res) => {
 
 const PORT = process.env.PORT || 5000;
 
-yuliSotnikov208471391RavitSuissa207386491App.listen(PORT, () => {
+app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
 });
